3a8445449daabfe8d0c89f193e7b013cc75ccb7b
jlowery <devnull@localhost>
2009-06-16 11:36:10 -0500

    [svn r277] fixed tabs. added check for date value to print out in ISO numberic format

--- Controls/Hidden.py
+++ Controls/Hidden.py
@@ -11,9 +11,15 @@
 # included LICENSE.txt file for more information. Copyright 2007 KCG.
 
 __all__ = ['Hidden']
+import datetime
 from FormControl import FormControl
 
 class Hidden(FormControl):
-	inputType = 'hidden'
-	visible = False
-	showCaption = False
+    inputType = 'hidden'
+    visible = False
+    showCaption = False
+
+    def draw(self):
+        if isinstance(self.value, datetime.date):
+            self.value = self.value.strftime("%Y%m%d")
+        return FormControl.draw(self)
